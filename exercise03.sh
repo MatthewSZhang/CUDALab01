98da97a976249387d4003a35bf6e23d574cb7210
mondus <p.richmond@sheffield.ac.uk>
2018-01-11 13:56:31 +0000

    Update exercise03.sh

--- exercise03.sh
+++ exercise03.sh
@@ -2,9 +2,9 @@
 # Request GPU resource
 #$ -l gpu=1
 # Use the flybrain project group
-#$ -P flybrain
+#$ -P rse-training
 #Use the flybrain queue
-#$ -q flybrain.q 
+#$ -q rse-training.q 
 
 # Call your CUDA executable
 ./training
